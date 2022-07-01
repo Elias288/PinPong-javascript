00bd9b1661e4207d9ffdeafb85f6a952d888bb6e
elias <bianchi.elias@gmail.com>
2022-06-30 21:03:40 -0300

    Practica MVC parte 6
    - Se agrego un puntaje a cada jugador que aumenta cada vez que la pelota toca la pared contraria.
    - Metodos de reseteo de valores a bola, barras y board que se ejecuta cuando termina el juego
    - Metodos de reseteo de posiciones a bola y barras.
    - Un metodo para detener el jugo.
    - Se agrego un mensaje de ganador cuando uno de los jugadores llega a 5 puntos.
    - Se agregaron estilos a la pagina.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
         this.game_over = false
         this.bars = []
         this.ball = null
+        this.winner = null
     }
 
     self.Board.prototype = {
@@ -15,6 +16,16 @@
             var elements = this.bars.map(function(bar){ return bar })
             elements.push(this.ball)
             return elements
+        },
+        reset: function(){ // cuando la bola hace un punto
+            this.ball.reset()
+            this.bars.map(b => b.reset())
+        },
+        reset_game: function(){ // cuando hay ganador
+            this.playing = true
+            this.game_over = false
+            this.bars.map(b => b.reset_score())
+            this.ball.reset()
         }
     }
 })();
@@ -50,7 +61,7 @@
         collisionBar: function(bar){
             var relative_intersect_y = ( bar.y + (bar.height / 2) ) - this.y
             
-            console.log("rel: "+ relative_intersect_y + " y: " + this.y);
+            
             var normalized_intersect_y = relative_intersect_y / (bar.height / 2)
 
             this.bounce_angle = normalized_intersect_y * this.max_bounce_angle
@@ -68,17 +79,25 @@
                 this.speed_y = this.speed * -Math.sin(this.max_bounce_angle)
                 this.speed_x = this.speed * Math.cos(this.max_bounce_angle)
             }
+        },
+        reset: function(){
+            this.x = 350
+            this.y = 100
+            this.bounce_angle = 0
         }
     }
 })();
 // BARRA
 (function(){
-    self.Bar = function(x, y, width, height, board){
+    self.Bar = function(id, x, y, width, height, board, scoreBar){
+        this.id = id
         this.x = x
         this.y = y
         this.width = width
         this.height = height
         this.board = board
+        this.score = 0
+        this.scoreBar = scoreBar
         // se acceede al array bars de board y se le agrega esta barra
         this.board.bars.push(this)
         // para saber que tiene que dibujar el canvasa
@@ -87,11 +106,33 @@
     }
 
     self.Bar.prototype = {
+        //movimiento de las barras
         down : function() {
-            if (this.board.playing && this.y + (this.height) < this.board.height) this.y += this.speed
+            if (this.board.playing && !this.board.game_over && this.y + (this.height) < this.board.height) 
+                this.y += this.speed
         },
         up : function() {
-            if(this.board.playing && this.y > 0) this.y -= this.speed
+            if(this.board.playing && !this.board.game_over && this.y > 0) 
+                this.y -= this.speed
+        },
+        scoreUp : function(){ // aumentar el puntaje de cada jugador
+            if(this.board.playing && !this.board.game_over) {
+                this.score ++   
+                this.scoreBar.innerHTML = this.score
+            } 
+            if(this.score == 5) {
+                this.board.game_over = true
+                alert('jugador ganador: ' + this.id)
+                this.board.reset_game()
+            }
+        },
+        reset: function(){
+            this.y = this.board.height / 2 - this.height / 2
+        },
+        reset_score: function(){
+            this.reset()
+            this.scoreBar.innerHTML = 0
+            this.score = 0
         }
     }
 })();
@@ -107,7 +148,8 @@
 
     self.BoardView.prototype = {
         clean: function(){
-            setTimeout(() => {this.ctx.clearRect(0, 0, this.board.width, this.board.height)}, 200)
+            setTimeout(() => {this.ctx.clearRect(0, 0, this.board.width, this.board.height)}, 100)
+            
         },
         draw: function(){
             for(var i = this.board.elements.length - 1; i >= 0; i--) {
@@ -118,7 +160,7 @@
         },
         play: function(){
             this.draw()
-            if (this.board.playing){
+            if (this.board.playing && !this.board.game_over){
                 this.clean()
                 this.check_collisions()
                 this.board.ball.move()
@@ -132,8 +174,16 @@
             }
 
             // chequea las colisiones que dan puntos
-            if (this.board.ball.x >= this.board.width || this.board.ball.x <= 0){
-                this.board.playing = false
+            if (this.board.ball.x >= this.board.width){
+                this.board.bars[0].scoreUp()
+                this.board.reset()
+                // console.log(this.board.bars[1].score);
+            }
+            
+            if (this.board.ball.x <= 0){
+                this.board.bars[1].scoreUp()
+                this.board.reset()
+                // console.log(this.board.bars[0].score);
             }
 
             // chequea la colisiones con las barras
@@ -143,7 +193,7 @@
                     this.board.ball.collisionBar(bar)
                 }
             }
-        }
+        },
     }
 
     function hitBarra(a, b){
@@ -203,8 +253,10 @@
 
 var board = new Board(800,400)
 var canvas = document.getElementById('canvas')
-var bar2 = new Bar(20, 100, 40, 100, board)
-var bar = new Bar(735, 100, 40, 100, board)
+var score1 = document.getElementById('jugador1')
+var score2 = document.getElementById('jugador2')
+var bar = new Bar(1, 735, 100, 40, 100, board, score1)
+var bar2 = new Bar(2, 20, 100, 40, 100, board, score2)
 var board_view = new BoardView(canvas, board)
 var ball = new Ball(350, 100, 10, board)
 
@@ -225,6 +277,8 @@ document.addEventListener("keydown", evt => {
         case "w":
             bar2.up()
             break;
+        case "p":
+            board.playing == false ? board.playing = true : board.playing = false
     }
 })
 
